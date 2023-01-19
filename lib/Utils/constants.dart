98ac9bf3bbb4705ded6d6839043955910064ecdd
Aryan <aryanbisht9458@gmail.com>
2023-01-19 09:35:08 +0530

    Constant File created.

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Color secondaryColor = Color(0xFF602467);
+Color primaryColor = Color(0xFFe7d9e9);
+
+extension ShowSnackBar on BuildContext {
+  void showSnackBar({
+    required String message,
+    Color backgroundColor = Colors.green,
+  }) {
+    ScaffoldMessenger.of(this)..removeCurrentSnackBar()..showSnackBar(SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+        maxLines: 2,
+      ),
+      backgroundColor: backgroundColor,
+    ));
+  }
+
+  void showErrorSnackBar({required String message}) {
+    showSnackBar(message: message, backgroundColor: Colors.red);
+  }
+}
