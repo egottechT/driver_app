aca5bc1e44f6599f575480ccb4f69dbf96a277f3
Aryan <aryanbisht9458@gmail.com>
2023-01-19 09:35:26 +0530

    Splash Screen created.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import 'package:driver_app/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      title: 'Book My taxi Driver',
+      routes: appPageRoutes,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.grey[800]),
+      ),
+      home: SplashScreen(),
+    ),
+  );
 }
 
+final Map<String, Widget Function(BuildContext)> appPageRoutes = {
+  'splash': (_) => const SplashScreen(),
+  // '/loginScreen': (_) => const LoginScreen(),
+  // '/phoneNumberSetup': (_) => const PhoneNumberSetup(),
+  // '/registrationScreen': (_) => const RegistrationScreen(),
+  // '/homeScreen': (_) => const HomeScreen(),
+  // '/mapScreen': (_) => MapsScreen(),
+  // '/permissionScreen': (_) => const PermissionScreen(),
+};
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'package:driver_app/Utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (await FirebaseAuth.instance.currentUser != null) {
+      // Navigator.of(context).pushReplacementNamed('/mapScreen');
+    } else {
+      // Navigator.of(context).pushReplacementNamed('/loginScreen');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: primaryColor,
+          body: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset(
+                        'assets/images/app_logo.png',
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset(
+                        'assets/images/splash_screen.png',
+                      ),
+                    ),
+                    flex: 2,
+                  )
+                ],
+              ))),
+    );
+  }
+}
