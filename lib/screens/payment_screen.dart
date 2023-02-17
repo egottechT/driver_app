b91c88642d10ed010ba257ae9e35a9fb720c3a7a
Aryan <aryanbisht9458@gmail.com>
2023-02-17 12:14:36 +0530

    UI is completed fully.

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -18,7 +18,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
           style: TextStyle(
               color: secondaryColor, fontSize: 24, fontWeight: FontWeight.bold),
         ),
-        SizedBox(
+        const SizedBox(
           height: 5,
         ),
         Text(title, style: TextStyle(color: secondaryColor, fontSize: 12))
@@ -36,63 +36,72 @@ class _PaymentScreenState extends State<PaymentScreen> {
       ),
       backgroundColor: Colors.grey[200],
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: primaryColor,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Column(
+            children: [
+              Container(
+                color: primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      cardViewLayout("Rs. 120", "TOTAL FARE"),
-                      cardViewLayout("3.5 km", "TOTAL DISTANCE"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          cardViewLayout("Rs. 120", "TOTAL FARE"),
+                          cardViewLayout("3.5 km", "TOTAL DISTANCE"),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 55),
+                        child: Text("Mr. Raj to Pay in Cash",
+                            style: TextStyle(
+                                color: secondaryColor,
+                                fontWeight: FontWeight.bold)),
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 55),
-                    child: Text("Mr. Raj to Pay in Cash",
-                        style: TextStyle(
-                            color: secondaryColor,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: locationAddress(),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: fareCollectionCard(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: locationAddress(),
-                  ),
-                ),
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: fareCollectionCard(),
-                  ),
-                ),ElevatedButton(
-                  onPressed: (){
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ElevatedButton(
+              onPressed: (){
 
-                  },
-                  child: Text("CASH COLLECTED",style: TextStyle(color: Colors.white),),
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                )
-              ],
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              child: const Text("CASH COLLECTED",style: TextStyle(color: Colors.white),),
             ),
-          ),
-
+          )
         ],
       ),
     );
@@ -103,13 +112,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
       mainAxisSize: MainAxisSize.min,
       children: [
         icon,
-        SizedBox(
+        const SizedBox(
           width: 5,
         ),
         Flexible(
             child: Text(
           address,
-          style: TextStyle(overflow: TextOverflow.clip),
+          style: const TextStyle(overflow: TextOverflow.clip),
         ))
       ],
     );
@@ -121,12 +130,12 @@ class _PaymentScreenState extends State<PaymentScreen> {
     return Column(
       children: [
         addressWithIcon(
-            Icon(
+            const Icon(
               Icons.circle_outlined,
               color: Colors.green,
             ),
             pickUp),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
         Divider(
@@ -134,7 +143,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
           height: 5,
           thickness: 2,
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
         addressWithIcon(
@@ -159,13 +168,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
             Text(price),
           ],
         ),
-        SizedBox(height: 5,),
+        const SizedBox(height: 5,),
         Divider(
           color: secondaryColor,
           height: 5,
           thickness: 2,
         ),
-        SizedBox(height: 5,),
+        const SizedBox(height: 5,),
       ],
     );
   }
