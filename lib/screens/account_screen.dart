5bc2dae353e37c74f034cd061a89099e1fce7f4d
Aryan <aryanbisht9458@gmail.com>
2023-03-07 13:43:59 +0530

    Car Detail Screen added.

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -23,41 +23,42 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          firstCardView("Name", "Manage profile"),
-          Divider(
-            height: 0,
-            thickness: 3,
-            color: secondaryColor,
-          ),
-          ListView.builder(
-            itemBuilder: (context, index) {
-              return cardViewWithText(values[index].first,index);
-            },
-            itemCount: values.length,
-            shrinkWrap: true,
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            firstCardView("Name", "Manage profile"),
+            Divider(
+              height: 0,
+              thickness: 3,
+              color: secondaryColor,
+            ),
+            ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+              primary: false,
+              itemBuilder: (context, index) {
+                return cardViewWithText(values[index].first,index);
+              },
+              itemCount: values.length,
+              shrinkWrap: true,
+            )
+          ],
+        ),
       ),
     );
   }
 
   cardViewWithText(String title,int index) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 2),
-      child: GestureDetector(
-        onTap: values[index].last,
-        child: Card(
-          color: primaryColor,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Row(
-              children: [
-                Icon(Icons.play_arrow_rounded,color: secondaryColor),
-                Text(title,style: TextStyle(fontWeight: FontWeight.bold,color: secondaryColor),)],
-            ),
+    return GestureDetector(
+      onTap: values[index].last,
+      child: Card(
+        color: primaryColor,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Row(
+            children: [
+              Icon(Icons.play_arrow_rounded,color: secondaryColor),
+              Text(title,style: TextStyle(fontWeight: FontWeight.bold,color: secondaryColor),)],
           ),
         ),
       ),
---
+++ lib/screens/car_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CarDetailScreen extends StatefulWidget {
+  const CarDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CarDetailScreen> createState() => _CarDetailScreenState();
+}
+
+class _CarDetailScreenState extends State<CarDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
