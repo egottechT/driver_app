3c120b3427446f79c752c3924c7448910653d330
Aryan <aryanbisht9458@gmail.com>
2023-03-16 11:30:09 +0530

    destination address is done.

--- lib/screens/pickup_screens/bottom_panel.dart
+++ lib/screens/pickup_screens/bottom_panel.dart
@@ -1,9 +1,9 @@
 import 'package:driver_app/Utils/constants.dart';
-import 'package:driver_app/screens/setting_screens/payment_screen.dart';
 import 'package:driver_app/screens/pickup_screens/pick_otp_check.dart';
+import 'package:driver_app/screens/setting_screens/payment_screen.dart';
 import 'package:flutter/material.dart';
 
-Widget bottomPanelLayout(Map map,BuildContext context,bool isPickup) {
+Widget bottomPanelLayout(Map map, BuildContext context, bool isPickup) {
   return Container(
     color: Colors.grey[200],
     padding: const EdgeInsets.all(10.0),
@@ -14,40 +14,72 @@ Widget bottomPanelLayout(Map map,BuildContext context,bool isPickup) {
           "Pick-up",
           style: TextStyle(color: secondaryColor, fontWeight: FontWeight.bold),
         ),
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         Container(
             decoration: BoxDecoration(
                 color: Colors.white, border: Border.all(color: secondaryColor)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
               children: [
-                Image.asset("assets/images/car.png"),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const Text("Customer Details",style: TextStyle(fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 5,),
-                    Text(map["title"] ?? ""),
+                    Image.asset("assets/images/car.png"),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Customer Details",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(map["title"] ?? ""),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Call to Customer",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(map["phoneNumber"] ?? ""),
+                      ],
+                    )
                   ],
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Call to Customer",style: TextStyle(fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 5,),
-                    Text(map["phoneNumber"] ?? ""),
+                    const Text(
+                      "Destination Address",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(map["destination"]["location"] ?? ""),
                   ],
-                )
+                ),
               ],
             )),
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         ElevatedButton(
-          onPressed: (){
-            if(isPickup) {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>PickOtpScreen(map: map)));
-            }
-            else{
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>PaymentScreen(map: map)));
+          onPressed: () {
+            if (isPickup) {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => PickOtpScreen(map: map)));
+            } else {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => PaymentScreen(map: map)));
               context.showErrorSnackBar(message: "Working on this sir");
             }
           },
